65acd995a8ea475076417ac4f51cfe6ccb8e2c4f
cnyan <cnyanmail@163.com>
2019-07-24 14:19:10 +0800

    update

--- actionRecognition1/input_data.py
+++ actionRecognition1/input_data.py
@@ -13,10 +13,11 @@
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
+from sklearn import preprocessing
 import os
 
 # 文件路径
-ACTION_ROOT_PATH = 'D:\\temp\\actionData'
+ACTION_ROOT_PATH = 'D:\\temp\\action_windows'
 
 
 # ER 表示开头序号
@@ -39,11 +40,11 @@ def save_data_sets(SHOW_PLOT=False):
         datamat = []
         labels = []
 
-        lab = int(filename.split('\\')[-1].split('.')[0]) - 1
+        lab = int(filename.split('_')[-1].split('.')[0]) - 1
         label = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         label[lab] = 1
 
-        df = np.array(pd.read_csv(filename, dtype=float)).round(6)[:, 1:]
+        df = np.array(pd.read_csv(filename, dtype=float)).round(6)[:, 1:-1]
         df = df[:int(len(df) / 30) * 30, :]
 
         data = np.reshape(df, (-1, 30, 63))
@@ -53,20 +54,19 @@ def save_data_sets(SHOW_PLOT=False):
             labels.append(label)
         # print(np.array(datamat).shape)
         # print(np.array(labels).shape)
+        # 横向组合数据与标签
         complex_array = np.concatenate([np.array(datamat), np.array(labels)], axis=1)
         # print(complex_array.shape)
+        # 纵向累加数据
         action_array = np.concatenate([action_array, complex_array], axis=0)
         # action_dict = {'data': np.array(datamat), 'labels': np.array(labels)}
         # print(action_dict['data'])
         # print('======' * 5)
 
-    # print(action_array.shape)
-    print(action_array[1, :-11])
-    print(action_array[1, -10:])
+    print(action_array.shape)  # (11077, 1900)  1890+10
+
     # 随机排序
     np.random.shuffle(action_array)
-    print(action_array[1, :-11])
-    print(action_array[1, -10:])
 
     # action_data = {'data': action_array[:, :-11], 'labels': action_array[:, -10:], 'original_data': action_array}
     # print(action_data['data'])
@@ -81,16 +81,26 @@ def read_data_sets(file_path='src/actions_data.npy', test_size=0.3):
     :return:
     '''
     df_array = np.load(file_path)
+    df_data = df_array[:, :-10]
+    df_labels = df_array[:, -10:]
+
+    # 对数据进行归一化处理
+    df_data = preprocessing.normalize(df_data)
+    # print(df_data.shape)
+    # print(df_labels.shape)
+    df_array = np.concatenate([df_data, df_labels], axis=1)
+    # print(df_array[0, :10])
+    # 重新排序
     np.random.shuffle(df_array)
 
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(df_array[:, :-11], df_array[:, -10:], test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test_split(df_array[:, :-10], df_array[:, -10:], test_size=test_size)
 
     np.random.shuffle(df_array)
     validation = df_array[:int(len(df_array) * test_size)]
-    X_validation, y_validation = validation[:, :-11], validation[:, -10:]
+    X_validation, y_validation = validation[:, :-10], validation[:, -10:]
 
-    action_data = {'all_data': df_array[:, :-11], 'all_labels': df_array[:, -10:], 'train_data': X_train,
+    action_data = {'all_data': df_array[:, :-10], 'all_labels': df_array[:, -10:], 'train_data': X_train,
                    'test_data': X_test, 'train_label': y_train, 'test_label': y_test, 'validation_data': X_validation,
                    'validation_label': y_validation, 'original_data': df_array}
     return action_data
@@ -98,4 +108,20 @@ def read_data_sets(file_path='src/actions_data.npy', test_size=0.3):
 
 if __name__ == '__main__':
     # save_data_sets()
-    read_data_sets()
+    actions_data = read_data_sets(test_size=0.1)
+    print('总的数据集:' + str(len(actions_data['original_data'])) + '行')
+    print('训练数据集{}'.format(actions_data['train_data'].shape))
+    print(type(actions_data))
+    print(actions_data['all_data'][0])
+   # print(actions_data['all_data'].shape)
+   # print(actions_data['all_labels'].shape)
+
+    # print(actions_data['train_data'].shape)
+
+'''
+  for i in range(20):
+    xs, ys = next_batch_data(batch_size=100, actions_set=actions_data)
+    print(xs)
+    print(ys)
+    print('========' * 5)
+'''
--- actionRecognition1/train_action.py
+++ actionRecognition1/train_action.py
@@ -7,15 +7,137 @@
 @License: Apache Licence
 @CreateTime: 2019/7/20 11:04
 @Describe：
-
+    基于多层感知机 两层深度网络实现训练动作分类
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+import tensorflow as tf
 from actionRecognition1 import input_data
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # 数据处理
+actions_data = input_data.read_data_sets('src/actions_data.npy', test_size=0.1)
 
-actions_data = input_data.read_data_sets('src/actions_data.npy')
-print(actions_data['all_data'][1])
-print(actions_data['all_labels'][:20])
-print(actions_data['validation_data'])
-print(actions_data['validation_label'])
+'''定义相关参数(超参数)'''
+batch_size = 100  # 设置每一轮训练的batch大小
+learning_rate = 0.8  # 设置学习率
+learning_rate_decay = 0.999  # 学习率的衰减系数,一般设置为非常接近于1的值
+max_steps = 30000  # 最大训练步数
+training_step = tf.Variable(0, trainable=False)
 
+'''定义全连接的前向网络，定义得到一个隐藏层和输出层的前向传播计算方式，激活函数用relu'''
+
+
+def hidden_layer(input_tensor, weights1, bias1, weights2, bias2, layer_name):
+    layer1 = tf.nn.relu(tf.matmul(input_tensor, weights1) + bias1)
+    outLayer = tf.matmul(layer1, weights2) + bias2
+    return outLayer
+
+
+import numpy as np
+
+
+def next_batch_data(batch_step, batch_size, actions_set):
+    actions_data = actions_set['train_data']
+    actions_label = actions_set['train_label']
+
+    complex_array = np.concatenate([actions_data, actions_label], axis=1)
+    if ((batch_step + 1) * batch_size) < len(actions_data):
+
+        return complex_array[batch_step * batch_size:(batch_step + 1) * batch_size, :-10], \
+               complex_array[batch_step * batch_size:(batch_step + 1) * batch_size, -10:]
+    else:
+        np.random.shuffle(complex_array)
+        return complex_array[: batch_size, :-10], complex_array[: batch_size, -10:]
+
+
+# 开始
+x = tf.compat.v1.placeholder(tf.float32, [None, 1890], name='x-input')  # 输入数据
+y_ = tf.compat.v1.placeholder(tf.float32, [None, 10], name='y-output')  # 标签
+
+# 生成隐藏层参数，隐藏层有1890个神经单元，其中weights包含1890*500个参数
+weights1 = tf.Variable(tf.random.truncated_normal([1890, 500], stddev=0.1))
+bias1 = tf.Variable(tf.constant(0.1, shape=[500]))
+# 生成输出层参数，输出层10个神经元，weights2包含500*10 = 5000个参数
+weights2 = tf.Variable(tf.random.truncated_normal([500, 10], stddev=0.1))
+bias2 = tf.Variable(tf.constant(0.1, shape=[10]))
+
+# 开始计算经过神经网络前向传播后得到的y值
+y = hidden_layer(x, weights1, bias1, weights2, bias2, 'y')
+
+'''为了提高采用随机梯度下降算法的训练神经网络得到的模型在测试集上的表现，
+    使用“滑动平均模型”应用到隐藏层与输出层的权重参数与偏置参数
+    使用“滑动平均模型”计算影子变量
+'''
+# 初始化一个"滑动平均类"，衰减率为0.99
+# 为了使这个训练前期可以更新得更快，这里提供了num_updates参数，并设置为当前网络的训练轮数
+averages_class = tf.train.ExponentialMovingAverage(decay=0.99, num_updates=training_step)
+# 定义一个更新变量滑动平均值的操作，需要向滑动平均类的apply()函数提供一个参数列表
+# train_variables() 函数返回集合图上 Graph.TRAINABLE_VARIABLES中的元素，这个集合的元素就是所有没有指定trainable=false的元素
+averages_op = averages_class.apply(tf.compat.v1.trainable_variables())  # 应用到集合图上所有权重参数和偏置参数，随着每次迭代动态改变参数值
+
+# 再次计算经过神经网络前向传播后得到的y的值，这里使用了滑动平均，但要牢记滑动平均值只是一个影子变量
+average_y = hidden_layer(x, weights1, bias1, weights2, bias2, 'average_y')
+
+'''开始计算交叉熵损失函数'''
+# 计算交叉熵函数J（w），logits是前向传播的结果，axis=1 按行获取最大值的下标，即对应识别的结果
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, axis=1))
+# 正则化 λR(w)
+regularizer = tf.contrib.layers.l2_regularizer(0.0001)
+regularization = regularizer(weights1) + regularizer(weights2)
+# 总的损失函数= J（w）+ λR(w)
+loss = tf.reduce_mean(cross_entropy) + regularization
+
+''' 使用随机梯度下降SGD优化器，学习率采用指数衰减的方式
+    learning_rate: 学习率 0.8
+    global_step：当前训练次数
+    decay_steps：衰减速度
+    decay_rate：衰减系数，小于1的较大数 0.99
+'''
+# 设置指数衰减法来设置学习率，staircase=false,即学习率连续衰减
+learning_rate = tf.compat.v1.train.exponential_decay(learning_rate=learning_rate, global_step=training_step,
+                                                     decay_steps=100, decay_rate=learning_rate_decay, staircase=True)
+train_op_step = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss=loss,
+                                                                                                  global_step=training_step)
+
+'''
+    在训练这个模型时，每过一遍数据，既要通过反向传播来更新神经网络中的参数，又需要更新每一个参数的滑动平均值
+    control_dependencies()用于完成这样的一次性的多次操作
+    # 下面两行代码，可以用一行代码替换
+    # train_op = tf.group(train_op_step,averages_op)
+'''
+with tf.control_dependencies([train_op_step, averages_op]):
+    train_op = tf.no_op(name='train')  # 什么都不做，仅做为点位符使用控制边界。
+
+'''检查使用了滑动平均值模型的神经网络前向传播是否正确，平均值就是模型在这一组数据上的正确率，求准确率'''
+current_prediction = tf.equal(tf.argmax(average_y, axis=1), tf.argmax(y_, axis=1))
+accuracy = tf.reduce_mean(tf.cast(current_prediction, tf.float32))  # tf.cast()这里将布尔类型变为float32类型的数据
+
+''' 构建会话 '''
+init_op = tf.compat.v1.global_variables_initializer()
+with tf.compat.v1.Session() as sess:
+    sess.run(init_op)
+
+    # 准备验证数据
+    validate_feed = {x: actions_data['validation_data'], y_: actions_data['validation_label']}
+    # 准备测试数据
+    test_feed = {x: actions_data['test_data'], y_: actions_data['test_label']}
+
+    for i in range(max_steps):
+        # 每训练1000次计算滑动模型在验证数据集上的结果
+        if i % 1000 == 0:
+            validate_accuracy = sess.run(accuracy, feed_dict=validate_feed)
+            print('After % d training step(s),validation accuracy using average model is %g%%' % (
+                i, validate_accuracy * 100))
+
+        # 产生这一轮的batch训练数据，并进行训练
+        xs, ys = next_batch_data(batch_step=i, batch_size=batch_size, actions_set=actions_data)
+        sess.run(train_op, feed_dict={x: xs, y_: ys})
+
+    # 得到正确率
+    test_accuracy = sess.run(accuracy, feed_dict=test_feed)
+    print("After %d training step(s)，test accuracy using average model is %g%%" % (max_steps, test_accuracy * 100))
